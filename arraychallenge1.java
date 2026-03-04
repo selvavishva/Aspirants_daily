539997cffd600837784156613e3d794c1ba74c73
selva_vishva_M <selvavishva24@gmail.com>
2026-03-04 20:39:32 +0530

    Arraybasic1

---
+++ arraychallenge1.java
@@ -0,0 +1,10 @@
+public class arraychallenge1 {
+    public static void main(String[] args) {
+        
+        int[] num ={10,20,30,40,50,60,70,8,9,10};
+
+        for(int i=0;i<10;i++){
+            System.out.println(num[i]);
+        }
+    }
+}
