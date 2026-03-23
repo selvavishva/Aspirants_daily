97bfa1d9829170658a5340936e83ee8ef70c9e74
selva_vishva_M <selvavishva24@gmail.com>
2026-03-23 21:32:25 +0530

    if basic

---
+++ giftask4.java
@@ -0,0 +1,20 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class gif4{
+    public static void main(String[] args) {
+         Scanner gif = new Scanner(System.in);
+         String surya =gif.nextLine();
+       //  String surya ="dead";
+
+
+         if (surya.equals("dead")) {
+
+                  System.out.println("megna dead");            
+         }
+         else
+         {
+            System.out.println("surya weds megna");
+         }
+    }
+}
