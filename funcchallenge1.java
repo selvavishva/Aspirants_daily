972cf55e758446e74d2b727d3f84085e9adf6dcf
selva_vishva_M <selvavishva24@gmail.com>
2026-03-27 21:24:53 +0530

    Function challenge

---
+++ funcchallenge1.java
@@ -0,0 +1,14 @@
+public class Funcchallenge1 {
+    
+    int apple_price=20;
+    int apple_count=5;
+
+    void money(){
+        System.out.println("Total amount:"+apple_price*apple_count);
+    }
+        public static void main(String[] args) {
+            
+            Funcchallenge1 obj1 = new Funcchallenge1();
+            obj1.money();
+        }
+    }
