30d58f6e927299191236829bf2afce99cd606412
selva_vishva_M <selvavishva24@gmail.com>
2026-04-27 21:16:44 +0530

    if basic task 7

---
+++ giftask7.java
@@ -0,0 +1,18 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class gi7{
+    public static void main(String[] args) {
+       
+         boolean mutton  = false;
+        boolean biriyani = false;
+
+         
+        if(mutton || biriyani) {
+            System.out.println("play");
+            
+        } else {
+            System.out.println("DON'PLAY");
+        }
+    }
+}
