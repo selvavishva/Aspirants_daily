abc0b0ef9a71240e02f58bc50d9023dbe6c46420
selva_vishva_M <selvavishva24@gmail.com>
2026-04-28 22:25:16 +0530

    if else basic 8

---
+++ giftask8.java
@@ -0,0 +1,26 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class gif8{
+    public static void main(String[] args) {
+       
+  /*        
+      int num = 15;     
+        if(num%3==0 && num%5==0) {
+            System.out.println("yess");     // comparison
+            
+        } else {
+            System.out.println("no");
+        }
+   */
+  int num=12;
+  if (num%2==0) {
+    System.out.println("EVEN");
+  }
+   else 
+  {
+    System.out.println("ODD");
+  }
+
+    }
+}
