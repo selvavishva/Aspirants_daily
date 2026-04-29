61547f2d368da4f04c14e60075ddf9d4879f2143
selva_vishva_M <selvavishva24@gmail.com>
2026-04-29 21:21:18 +0530

    if else basic 5

---
+++ giftask5.java
@@ -0,0 +1,19 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class gif5{
+    public static void main(String[] args) {
+       
+        // Scanner gif = new Scanner(System.in);
+        
+        ///int mark = gif.nextInt();
+
+      int mark = 35;     
+        if(mark>=35) {
+            System.out.println("passs");
+            
+        } else {
+            System.out.println("fail");
+        }
+    }
+}
