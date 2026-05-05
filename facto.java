c1679af43d412b45600dbb0a91b2e6435a9a7940
selva_vishva_M <selvavishva24@gmail.com>
2026-05-05 18:19:22 +0530

    Factorial program

---
+++ facto.java
@@ -0,0 +1,11 @@
+public class facto {
+    public static void main(String [] args){
+       
+        int num =7;
+        int fact =1;
+        for(int i=1;i<=num;i++){
+            fact = fact *i;
+        }
+        System.out.print(fact);
+    }
+}
