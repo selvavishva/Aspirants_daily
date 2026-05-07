b0490f0f71bf047bcee23e9951f5ebce9c705796
selva_vishva_M <selvavishva24@gmail.com>
2026-05-07 20:59:49 +0530

    if else basic task 3

---
+++ gifbasic3.java
@@ -0,0 +1,19 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class gif{
+    public static void main(String args[]){
+    Scanner gif = new Scanner(System.in);
+    int a = gif.nextInt();
+    int b = gif.nextInt();
+
+    if(a>b)
+    {
+        System.out.println("a is greter num");
+    }
+    else
+    {
+        System.out.println("b is greater num");
+    }
+}
+}
