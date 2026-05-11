8e8bbf7f575eac6a7539c8ad3119453da6167937
selva_vishva_M <selvavishva24@gmail.com>
2026-05-11 21:08:15 +0530

    Break basic 1

---
+++ breakbase.java
@@ -0,0 +1,10 @@
+public class breakbase {
+   public static void main(String[] args){
+       for(int i=1;i<55;i++){
+           if (i==10)
+               break;
+           System.out.println(i);
+       }
+       System.out.print("outside loop");
+   }
+}
