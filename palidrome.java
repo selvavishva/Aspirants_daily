968ed0dc407a60aec7171dd9f09107920dc31eeb
selva_vishva_M <selvavishva24@gmail.com>
2026-07-14 22:33:38 +0530

    palindrome or not

---
+++ palidrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int temp = num;
+        int rev = 0;
+
+        while (num > 0) {
+            int rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+        }
+
+        if (temp == rev) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
+}
